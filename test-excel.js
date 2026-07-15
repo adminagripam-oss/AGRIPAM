baf877ccaed348c2f8c7f73e9ae34a094a0f50b1
adminagripam-oss <admin.agripam@gmail.com>
2026-07-15 11:31:36 +0700

    chore: Remove temporary test files

--- test-excel.js
+++
@@ -1,9 +0,0 @@
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('Export_LHP_2026-07-13 (1).xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const rows = XLSX.utils.sheet_to_json(sheet, { header: 1, defval: '', blankrows: true });
-
-console.log('Row 14 (Aceh):');
-console.log(rows[14]);
-console.log('Row 38 (Sumut 1):');
-console.log(rows[38]);
--- test-excel2.js
+++
@@ -1,8 +0,0 @@
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('Export_LHP_2026-07-13 (1).xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const rows = XLSX.utils.sheet_to_json(sheet, { header: 1, defval: '', blankrows: true });
-
-for(let i=10; i<=20; i++) {
-    console.log('Row ' + (i+1) + ': ' + rows[i].slice(0, 10).join(' | '));
-}
